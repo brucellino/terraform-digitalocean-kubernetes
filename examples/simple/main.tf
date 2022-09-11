b360634ac91daf68e6ae228aacfe42481397d8ac
Bruce Becker <brucellino@protonmail.com>
2022-09-11 09:15:55 +0200

    test: initial commit of terratest
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,21 +1,54 @@
-# This is the default example
-# customise it as you see fit for your example usage of your module
-
-# add provider configurations here, for example:
-# provider "aws" {
-#
-# }
-
-# Declare your backends and other terraform configuration here
-# This is an example for using the consul backend.
-# terraform {
-#   backend "consul" {
-#     path = "test_module/simple"
-#   }
-# }
-
-
-module "example" {
-  source = "../../"
-  dummy  = "test"
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.22.2"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.8.2"
+    }
+  }
+  required_version = ">=1.2.0"
+  backend "consul" {
+    path = "tfmod_digitalocean_k8s"
+  }
 }
+
+# This doesn't seem to work?
+data "vault_kv_secret_subkeys_v2" "do_token" {
+  mount = "digitalocean"
+  name  = "tokens"
+}
+
+data "vault_generic_secret" "do_token" {
+  path = "digitalocean/tokens"
+}
+
+provider "digitalocean" {
+  # token = data.vault_kv_secret_subkeys_v2.do_token.data["terraform"]
+  token = data.vault_generic_secret.do_token.data["terraform"]
+}
+
+
+module "vpc" {
+  source  = "brucellino/vpc/digitalocean"
+  version = "1.0.2"
+  project = {
+    description = "Kubernetes testing",
+    environment = "Development",
+    name        = "K8s_test",
+    purpose     = "personal"
+  }
+  vpc_name   = "k8s"
+  vpc_region = "ams3"
+}
+
+
+
+module "k8s" {
+  depends_on = [module.vpc]
+  source     = "../../"
+  dummy      = "test"
+}
+
---
+++ test/k8sdo_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDigitalOceanVPC(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(
+		t, &terraform.Options{
+			TerraformDir: "../examples/simple/",
+			PlanFilePath: "test.tfplan",
+			Lock: true,
+		})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndPlanAndShow(t, terraformOptions)
+	output := terraform.OutputAll(t, terraformOptions)
+	assert.NotNil(t, output)
+	
+}
